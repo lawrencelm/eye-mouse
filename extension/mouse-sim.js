60ecec6d80cb9ef3654ee51a180551631350d464
Sydney Li <sydli@stanford.edu>
2014-04-13 02:14:00 -0700

    linking issue fixed. Squint eyes to slow down.

--- extension/mouse-sim.js
+++ extension/mouse-sim.js
@@ -35,7 +35,7 @@ var x = prevx;
 var y = prevy;
 var simx = x;
 var simy = y;
-var amp = 50;
+var amp = 30;
 
 var running_xavg = new Array();
 var running_yavg = new Array();
@@ -67,15 +67,17 @@ function highlight_nearest_link(x, y) {
   }*/
 }
 
+var clicked = false;
 function update_cursor(x, y) {
-  document.getElementById("eye-mouse-cursor").style.backgroundColor = "#0f0";// = "url("+chrome.extension.getURL("cursor_images/standard.gif") + ")";
-  document.getElementById("eye-mouse-cursor").style.top=(y+2).toString() + "px";
-  document.getElementById("eye-mouse-cursor").style.left=(x+2).toString() + "px";
+  if (document.getElementById("eye-mouse-cursor") == undefined) return;
+  if (!clicked) document.getElementById("eye-mouse-cursor").style.backgroundColor = "#0f0";// = "url("+chrome.extension.getURL("cursor_images/standard.gif") + ")";
+  document.getElementById("eye-mouse-cursor").style.top=(y+1).toString() + "px";
+  document.getElementById("eye-mouse-cursor").style.left=(x+1).toString() + "px";
   check_for_scroll(x, y);
   highlight_nearest_link(x, y);
 }
 
-var min = 1;
+var min = 5;
 var xmax = $(window).innerWidth()-1;
 var ymax = $(window).innerHeight()-1;
 var gaze_center =  new camgaze.structures.Point(xmax/2, ymax/2);
@@ -104,8 +106,8 @@ function move_from_centroid(c) {
   prevx = x; prevy = y;
   x = c.getX();
   y = c.getY();
-  simx -= (c.getX() - prevx) * amp;
-  simy += (c.getY() - prevy) * amp;
+  simx -= (x - prevx) * amp;
+  simy += (y - prevy) * amp;
   bounds_check();
   running_xavg.push(simx);
   running_xavg.shift();
@@ -118,15 +120,18 @@ function move_from_centroid(c) {
 
   function wink(side) {
     //console.log(simx, simy);
-    $("#eye-mouse-cursor").css("backgroundColor" , "#f00");
     var e = document.elementFromPoint(simx, simy);
-    if (typeof e.onclick == "function")
-      e.onclick.apply(e);
-    if (typeof e.mousedown == "function") e.mousedown();
-    if (e != null && e.tagName == "A" && e.hasAttribute("HREF")) window.location.replace(e.getAttribute("HREF"));
+    update_cursor(simx, simy);
+    //if (typeof e.onclick == "function")
+    //  e.onclick.apply(e);
+    //if (typeof e.mousedown == "function") e.mousedown();
+    if (e != null && e.tagName == "A" && e.hasAttribute("HREF")) {
+      clicked = true;
+      window.location.replace(e.getAttribute("HREF"));
+      $("#eye-mouse-cursor").css("background-color","#00f");
+    }
   }
 $(window).load(function() {
   update_cursor(simx, simy);
-
 });
 
